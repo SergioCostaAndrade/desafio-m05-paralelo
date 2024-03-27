99db3dcb6dd0825dc088237d9087462fdb416cd2
SergioCostaAndrade <scandrade1968@gmail.com>
2024-03-27 10:17:53 -0300

    deploy

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,22 @@
+//const knex = require("knex")({
+//  client: "pg",
+// connection: {
+//  host: "localhost",
+// user: "postgres",
+//password: "123456",
+//database: "pdv",
+// },
+//});
+//module.exports = knex;
 const knex = require("knex")({
-    client: "pg",
-    connection: {
-      host: "localhost",
-      user: "postgres",
-      password: "123456",
-      database: "pdv",
-    },
-  });
-  module.exports = knex;
-  
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
+  },
+});
+module.exports = knex;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const rota = require("./rotas");
+const cors = require("cors");
 
 const app = express();
 
