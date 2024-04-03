e822f29982c8a46eb78297b2f1b52101f40dfddc
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-02 23:25:03 -0300

    editar clientes e rotas

---
+++ src/controladores/editarcliente.js
@@ -0,0 +1,55 @@
+const knex = require('../conexao')
+
+const editarCliente = async (req, res) => {
+  const { id } = req.params
+
+  const { nome, email, cpf } = req.body
+
+  if (!nome || !email || !cpf) {
+    return res
+      .status(400)
+      .json({ mensagem: 'Nome, email e cpf são obrigatórios.' })
+  }
+
+  try {
+    const verificaID = await knex('clientes').where({ id }).first()
+
+    if (!verificaID) {
+      return res.status(400).json({
+        messagem: 'Cliente não identificado'
+      })
+    }
+
+    const verificaEmail = await knex('clientes').where({ email }).first()
+
+    if (verificaEmail) {
+      if (verificaEmail.id !== verificaID.id) {
+        return res.status(400).json({
+          messagem: 'Já existe usuário cadastrado com o e-mail informado.'
+        })
+      }
+    }
+
+    const verificaCPF = await knex('clientes').where({ cpf }).first()
+
+    if (verificaCPF) {
+      if (verificaCPF.id !== verificaID.id) {
+        return res.status(400).json({
+          messagem: 'Já existe usuário cadastrado com o CPF informado.'
+        })
+      }
+    }
+
+    const atualiza = await knex('clientes')
+      .update({ nome, email, cpf })
+      .where({ id })
+      .returning('*')
+
+    return res.status(200).json(atualiza)
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+  }
+}
+
+module.exports = editarCliente
--- src/rotas.js
+++ src/rotas.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const criarUsuario = require("./controladores/criarusuario");
-const listarCategorias = require("./controladores/listarcategorias");
-const efetuarLogin = require("./controladores/efetuarlogin");
-const editarUsuario = require("./controladores/editarusuario");
-const detalharUsuario = require("./controladores/detalharusuario");
-const usuarioLogado = require("./middwares/autenticador");
-const validarRequisicao = require("./middwares/validarRequisicao");
-const schemaUsuario = require("./validacao/schemaUsuario");
-const cadastrarProduto = require("./controladores/cadastrarproduto");
-const editarProduto = require("./controladores/editarproduto");
-const excluirProduto = require("./controladores/excluirproduto");
-const listarProdutos = require("./controladores/listarprodutos");
-const detalharProduto = require("./controladores/detalharproduto");
-const cadastrarCliente = require("./controladores/cadastrarcliente");
+const express = require('express')
+const criarUsuario = require('./controladores/criarusuario')
+const listarCategorias = require('./controladores/listarcategorias')
+const efetuarLogin = require('./controladores/efetuarlogin')
+const editarUsuario = require('./controladores/editarusuario')
+const detalharUsuario = require('./controladores/detalharusuario')
+const usuarioLogado = require('./middwares/autenticador')
+const validarRequisicao = require('./middwares/validarRequisicao')
+const schemaUsuario = require('./validacao/schemaUsuario')
+const cadastrarProduto = require('./controladores/cadastrarproduto')
+const editarProduto = require('./controladores/editarproduto')
+const excluirProduto = require('./controladores/excluirproduto')
+const listarProdutos = require('./controladores/listarprodutos')
+const detalharProduto = require('./controladores/detalharproduto')
+const cadastrarCliente = require('./controladores/cadastrarcliente')
+const editarCliente = require('./controladores/editarcliente')
 
-const rota = express();
+const rota = express()
 
-rota.post("/usuario", validarRequisicao(schemaUsuario), criarUsuario);
-rota.post("/login", validarRequisicao(schemaUsuario), efetuarLogin);
-rota.get("/categoria", listarCategorias);
-rota.use(usuarioLogado);
-rota.get("/usuario", detalharUsuario);
-rota.put("/usuario", editarUsuario);
-rota.post("/produto", cadastrarProduto)
+rota.post('/usuario', validarRequisicao(schemaUsuario), criarUsuario)
+rota.post('/login', validarRequisicao(schemaUsuario), efetuarLogin)
+rota.get('/categoria', listarCategorias)
+rota.use(usuarioLogado)
+rota.get('/usuario', detalharUsuario)
+rota.put('/usuario', editarUsuario)
+rota.post('/produto', cadastrarProduto)
 rota.put('/produto/:id', editarProduto)
 rota.delete('/produto/:id', excluirProduto)
-rota.post("/cliente", cadastrarCliente);
-rota.get("/produto",listarProdutos)
-rota.get("/produto/:id", detalharProduto)
-module.exports = rota;
+rota.post('/cliente', cadastrarCliente)
+rota.get('/produto', listarProdutos)
+rota.get('/produto/:id', detalharProduto)
+rota.put('/cliente/:id', editarCliente)
+module.exports = rota
