7e95764c212763be70e7fffabaad5d6dad8e181c
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 20:05:15 -0300

    row

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -26,7 +26,7 @@ const cadastrarPedido = async (req, res) => {
       observacao,
       valor_total: valorTotalPedido,
     });
-    console.log('tentativa', novoPedido.id);
+    console.log('tentativa', novoPedido.row[0].id);
     if (novoPedido.rowCount < 1) {
       return res.status(400).json({
         mensagem: "Pedido não cadastrado",
