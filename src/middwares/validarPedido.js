fa1f05a787fd57d40a0e4afd97076fd1c38a59a3
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 15:42:27 -0300

    incluindo middleware

---
+++ src/middwares/validarPedido.js
@@ -0,0 +1,80 @@
+const knex = require("../conexao");
+
+const validarPedido = async (req, res, next) => {
+  const { cliente_id, observacao, pedido_produtos } = req.body;
+  if (!cliente_id) {
+    return res.status(400).json({
+      mensagem:
+        "Deve ser informado a identificação do cliente para gerar o pedido",
+    });
+  }
+  let num = 0;
+  num = Number(cliente_id);
+  if (!Number.isInteger(num) || num <= 0) {
+    return res
+      .status(400)
+      .json({ mensagem: "Id informado não é inteiro ou é negativo" });
+  }
+  try {
+    const verificaID = await knex("clientes").where("id", cliente_id).first();
+
+    if (!verificaID) {
+      return res.status(400).json({ messagem: "Cliente não identificado" });
+    }
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+  if (!pedido_produtos) {
+    return res.status(400).json({
+      mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
+    });
+  }
+  if (pedido_produtos.length < 1) {
+    return res.status(400).json({
+      mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
+    });
+  }
+  for (const pedidoproduto of pedido_produtos) {
+    try {
+      if (!pedidoproduto.produto_id) {
+        return res.status(400).json({ messagem: "Produto não informado" });
+      }
+      num = 0;
+      num = Number(pedidoproduto.produto_id);
+      if (!Number.isInteger(num) || num <= 0) {
+        return res.status(400).json({
+          mensagem: "Id do produto informado não é inteiro ou é negativo",
+        });
+      }
+      if (!pedidoproduto.quantidade_produto) {
+        return res
+          .status(400)
+          .json({ messagem: "Quantidade do produto deve ser informada" });
+      }
+      num = 0;
+      num = Number(pedidoproduto.quantidade_produto);
+      if (!Number.isInteger(num) || num <= 0) {
+        return res.status(400).json({
+          mensagem:
+            "Quantidade do produto informada não é inteiro ou é negativo",
+        });
+      }
+      const verificaID = await knex("produtos")
+        .where("id", pedidoproduto.produto_id)
+        .first();
+      if (!verificaID) {
+        return res.status(400).json({ messagem: "Produto não identificado" });
+      }
+      if (verificaID.quantidade_estoque < pedidoproduto.quantidade_produto) {
+        return res.status(400).json({
+          messagem: "Quantidade em estoqe insuficiente para atender ao pedido",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "Erro interno do servidor" });
+    }
+  }
+  next();
+};
+module.exports = validarPedido;
--- src/rotas.js
+++ src/rotas.js
@@ -1,39 +1,40 @@
-const express = require('express')
-const criarUsuario = require('./controladores/criarusuario')
-const listarCategorias = require('./controladores/listarcategorias')
-const efetuarLogin = require('./controladores/efetuarlogin')
-const editarUsuario = require('./controladores/editarusuario')
-const detalharUsuario = require('./controladores/detalharusuario')
-const usuarioLogado = require('./middwares/autenticador')
-const validarRequisicao = require('./middwares/validarRequisicao')
-const schemaUsuario = require('./validacao/schemaUsuario')
-const cadastrarProduto = require('./controladores/cadastrarproduto')
-const editarProduto = require('./controladores/editarproduto')
-const excluirProduto = require('./controladores/excluirproduto')
-const listarProdutos = require('./controladores/listarprodutos')
-const detalharProduto = require('./controladores/detalharproduto')
-const cadastrarCliente = require('./controladores/cadastrarcliente')
-const editarCliente = require('./controladores/editarcliente')
-const listarClientes = require('./controladores/listarclientes')
-const detalharCliente = require('./controladores/detalharcliente')
-const cadastrarPedido = require('./controladores/cadastrarpedido')
+const express = require("express");
+const criarUsuario = require("./controladores/criarusuario");
+const listarCategorias = require("./controladores/listarcategorias");
+const efetuarLogin = require("./controladores/efetuarlogin");
+const editarUsuario = require("./controladores/editarusuario");
+const detalharUsuario = require("./controladores/detalharusuario");
+const usuarioLogado = require("./middwares/autenticador");
+const validarRequisicao = require("./middwares/validarRequisicao");
+const schemaUsuario = require("./validacao/schemaUsuario");
+const cadastrarProduto = require("./controladores/cadastrarproduto");
+const editarProduto = require("./controladores/editarproduto");
+const excluirProduto = require("./controladores/excluirproduto");
+const listarProdutos = require("./controladores/listarprodutos");
+const detalharProduto = require("./controladores/detalharproduto");
+const cadastrarCliente = require("./controladores/cadastrarcliente");
+const editarCliente = require("./controladores/editarcliente");
+const listarClientes = require("./controladores/listarclientes");
+const detalharCliente = require("./controladores/detalharcliente");
+const cadastrarPedido = require("./controladores/cadastrarpedido");
+const validarPedido = require("./middwares/validarPedido");
 
-const rota = express()
+const rota = express();
 
-rota.post('/usuario', validarRequisicao(schemaUsuario), criarUsuario)
-rota.post('/login', validarRequisicao(schemaUsuario), efetuarLogin)
-rota.get('/categoria', listarCategorias)
-rota.use(usuarioLogado)
-rota.get('/usuario', detalharUsuario)
-rota.put('/usuario', editarUsuario)
-rota.post('/produto', cadastrarProduto)
-rota.put('/produto/:id', editarProduto)
-rota.delete('/produto/:id', excluirProduto)
-rota.post('/cliente', cadastrarCliente)
-rota.get('/cliente', listarClientes)
-rota.get('/produto', listarProdutos)
-rota.get('/produto/:id', detalharProduto)
-rota.put('/cliente/:id', editarCliente)
-rota.get('/cliente/:id', detalharCliente )
-rota.post('/pedido', cadastrarPedido)
-module.exports = rota
+rota.post("/usuario", validarRequisicao(schemaUsuario), criarUsuario);
+rota.post("/login", validarRequisicao(schemaUsuario), efetuarLogin);
+rota.get("/categoria", listarCategorias);
+rota.use(usuarioLogado);
+rota.get("/usuario", detalharUsuario);
+rota.put("/usuario", editarUsuario);
+rota.post("/produto", cadastrarProduto);
+rota.put("/produto/:id", editarProduto);
+rota.delete("/produto/:id", excluirProduto);
+rota.post("/cliente", cadastrarCliente);
+rota.get("/cliente", listarClientes);
+rota.get("/produto", listarProdutos);
+rota.get("/produto/:id", detalharProduto);
+rota.put("/cliente/:id", editarCliente);
+rota.get("/cliente/:id", detalharCliente);
+rota.post("/pedido", validarPedido, cadastrarPedido);
+module.exports = rota;
