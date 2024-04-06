c7769225f1a5118210b075a356892f529a37f3ef
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 11:58:16 -0300

    entrada de dados

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -3,7 +3,31 @@ const knex = require("../conexao");
 const cadastrarPedido = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
   console.log(cliente_id, observacao, pedido_produtos);
+  if (!cliente_id) {
+    return res
+      .status(400)
+      .json({
+        mensagem:
+          "Deve ser informado a identificação do cliente para gerar o pedido",
+      });
+  }
+  try {
+    const verificaID = await knex("clientes").where("id", cliente_id).first();
 
-  return res.json("cadastrar pedido");
+    if (!verificaID) {
+      return res.status(400).json({ messagem: "Cliente não identificado" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+  if (!pedido_produtos) {
+    return res
+      .status(400)
+      .json({
+        mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
+      });
+  }
+  return res.json(cliente_id, observacao, pedido_produtos);
 };
 module.exports = cadastrarPedido;
