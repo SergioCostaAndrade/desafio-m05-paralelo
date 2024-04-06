2b2e7dd38dee02c9980b698e3c1336e8ea37c3f9
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 15:53:17 -0300

    valor total do pedido

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -2,7 +2,19 @@ const knex = require("../conexao");
 
 const cadastrarPedido = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
-
+  let valorTotalPedido = 0;
+  for (const pedidoproduto of pedido_produtos) {
+    try {
+      const verificaID = await knex("produtos")
+        .where("id", pedidoproduto.produto_id)
+        .first();
+      valorTotalPedido += pedidoproduto.quantidade_produto * verificaID.valor;
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ mensagem: "Erro interno do servidor" });
+    }
+  }
+  console.log(valorTotalPedido);
   return res.json(cliente_id, observacao, pedido_produtos);
 };
 module.exports = cadastrarPedido;
