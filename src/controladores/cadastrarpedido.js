cdca9c5db61e85daf4555c3ea721027a24393ae6
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 12:22:04 -0300

    teste de existencia do produto

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -4,12 +4,10 @@ const cadastrarPedido = async (req, res) => {
   const { cliente_id, observacao, pedido_produtos } = req.body;
   console.log(cliente_id, observacao, pedido_produtos);
   if (!cliente_id) {
-    return res
-      .status(400)
-      .json({
-        mensagem:
-          "Deve ser informado a identificação do cliente para gerar o pedido",
-      });
+    return res.status(400).json({
+      mensagem:
+        "Deve ser informado a identificação do cliente para gerar o pedido",
+    });
   }
   try {
     const verificaID = await knex("clientes").where("id", cliente_id).first();
@@ -22,19 +20,30 @@ const cadastrarPedido = async (req, res) => {
     return res.status(500).json({ mensagem: "Erro interno do servidor" });
   }
   if (!pedido_produtos) {
-    return res
-      .status(400)
-      .json({
-        mensagem: "Deve ser informado as informações de produto e quantidade do pedido",
-      });
+    return res.status(400).json({
+      mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
+    });
   }
   if (pedido_produtos.length < 1) {
-    return res
-      .status(400)
-      .json({
-        mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
-      });
+    return res.status(400).json({
+      mensagem: "Deve ser informado ao menos um produto para gerar o pedido",
+    });
   }
+  for (const pedidoproduto of pedido_produtos) {
+    try {
+      const verificaID = await knex("produtos")
+        .where('id', pedidoproduto.produto_id)
+        .first();
+
+      if (!verificaID) {
+        return res.status(400).json({ messagem: "Produto não identificado" });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ mensagem: "Erro interno do servidor" });
+    }
+  }
+
   return res.json(cliente_id, observacao, pedido_produtos);
 };
 module.exports = cadastrarPedido;
