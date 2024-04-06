d65ff59ccaee89259819b56377df5d61a2a3b643
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-06 11:29:27 -0300

    cadastro pedido

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -1,6 +1,9 @@
 const knex = require("../conexao");
 
 const cadastrarPedido = async (req, res) => {
+  const { cliente_id, observacao, pedido_produtos } = req.body;
+  console.log(cliente_id, observacao, pedido_produtos);
+
   return res.json("cadastrar pedido");
 };
 module.exports = cadastrarPedido;
