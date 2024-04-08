cac1b89839c408b68624fff42f20c61872678f78
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-08 17:16:03 -0300

    teste meikl

--- src/controladores/cadastrarpedido.js
+++ src/controladores/cadastrarpedido.js
@@ -83,6 +83,20 @@ const cadastrarPedido = async (req, res) => {
     //EMAIL_NAME=Equipe Atrasados e Unidos
     //EMAIL_FROM=scandrade@cubosacademy.com
     //
+    console.log(process.env.HOST_PORT,
+      process.env.KEY_DEVWEBTOKEN,
+      process.env.DB_HOST,
+      process.env.DB_PORT,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      process.env.DB_NAME,
+      process.env.EMAIL_HOST,
+      process.env.EMAIL_PORT,
+      process.env.EMAIL_USER,
+      process.env.EMAIL_PASS,
+      process.env.EMAIL_NAME,
+      process.env.EMAIL_FROM, 'cadastrar pedido');
+    //
     transportador.sendMail({
       //from: `${"Equipe Atrasados e Unidos"} <${"scandrade@cubosacademy.com"}>`,
       from: `Equipe Atrasados e Unidos <scandrade@cubosacademy.com>`,
