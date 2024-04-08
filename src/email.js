e3539bc5b42ff39c53c68c766b9d2e42f7f87333
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-08 20:04:27 -0300

    secure

--- src/email.js
+++ src/email.js
@@ -2,11 +2,6 @@ const nodemailer = require("nodemailer");
 const transportador = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: true,
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false,
-  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
