7fffb692729cad380710b3ad78718097dd82040c
SergioCostaAndrade <scandrade1968@gmail.com>
2024-04-08 19:31:49 -0300

    index sem log

--- src/index.js
+++ src/index.js
@@ -11,17 +11,4 @@ app.use(express.json());
 
 app.use(rota);
 
-console.log(process.env.HOST_PORT,
-    process.env.KEY_DEVWEBTOKEN,
-    process.env.DB_HOST,
-    process.env.DB_PORT,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    process.env.DB_NAME,
-    process.env.EMAIL_HOST,
-    process.env.EMAIL_PORT,
-    process.env.EMAIL_USER,
-    process.env.EMAIL_PASS,
-    process.env.EMAIL_NAME,
-    process.env.EMAIL_FROM, 'index');
 app.listen(process.env.HOST_PORT);
